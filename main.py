f71a8c9dd7ce871e4eee385e9c423399794a6fcf
Roshan Thomas Michael <roshanthomas337@gmail.com>
2025-05-17 16:14:00 -0500

    created the pop window and a full size canvas

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-msg = "Roll dice"
-print(msg)
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Radar")
+root.state('zoomed')
+w=tk.Canvas(root)
+w.pack(fill=tk.BOTH, expand = True)
+def dimension(event):
+    
+        width=event.width
+        height=event.height
+    
+tk.canvas.bind("<Configure>", dimension)
+radius = min(width, height) / 2
+x1 = height/2 - radius
+y1 = width/2 - radius
+x2 = height/2 + radius
+y2 = width/2 + radius
+w.create_oval(x1, y1, x2, y2, fill = "white")
+w.config(background="green")
+root.mainloop()
